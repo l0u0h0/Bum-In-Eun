ef0d04269ecf43d8e468212f57f69b01f7d24b6f
l0u0h0 <l0u0h0@gmail.com>
2022-08-30 03:02:04 +0900

    dict_main_update

--- frontend/src/components/dictionary/DicionarymainComponent.jsx
+++ frontend/src/components/dictionary/DicionarymainComponent.jsx
@@ -2,17 +2,47 @@ import React from "react";
 import { Card } from "react-bootstrap";
 import Banner from "../common/BannerComponent";
 import Header from "../common/HeaderComponent";
+import { Link } from "react-router-dom";
 
 export default function DictionaryMain() {
+  const list = [1, 2, 3, 4, 5];
   return (
     <div className="App-dictionarymain">
       <Header />
       <Card body className="dictionarymain-body">
         <Card body className="first-table">
           <h3 className="table-title">조회가 많았던 키워드</h3>
+          <table className="table-content">
+            <tbody>
+              {list.map((num) => (
+                <tr key={`table_row_${num}`}>
+                  <th className="data-rank">{num}.</th>
+                  <td className="data-word">
+                    <Link to="/crimeresult?category=gambling">
+                      <div className="data-word-link">킹받네</div>
+                    </Link>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
         </Card>
         <Card body className="second-table">
           <h3 className="table-title">추천이 많았던 키워드</h3>
+          <table className="table-content">
+            <tbody>
+              {list.map((num) => (
+                <tr key={`table_row_${num}`}>
+                  <th className="data-rank">{num}.</th>
+                  <td className="data-word">
+                    <Link to="/crimeresult?category=gambling">
+                      <div className="data-word-link">킹받네</div>
+                    </Link>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
         </Card>
       </Card>
       <Banner />
