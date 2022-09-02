b3720535ba865ab9510ae2a2e2e8f536c0994b78
l0u0h0 <l0u0h0@gmail.com>
2022-09-02 11:39:39 +0900

    static_link

--- frontend/src/components/statistic/StatisticmainComponent.jsx
+++ frontend/src/components/statistic/StatisticmainComponent.jsx
@@ -1,6 +1,7 @@
 // import
 import React, { useState, useEffect } from "react";
 import { Card } from "react-bootstrap";
+import { Link } from "react-router-dom";
 import Banner from "../common/BannerComponent";
 import Header from "../common/HeaderComponent";
 
@@ -12,28 +13,28 @@ export default function Dictionarydetail() {
     setData([
       {
         rank: 1,
-        word_1: "test-data",
-        word_2: "test-data_2",
+        word_1: "test-data_1_1",
+        word_2: "test-data_2_1",
       },
       {
         rank: 2,
-        word_1: "test-data",
-        word_2: "test-data_2",
+        word_1: "test-data_1_2",
+        word_2: "test-data_2_2",
       },
       {
         rank: 3,
-        word_1: "test-data",
-        word_2: "test-data_2",
+        word_1: "test-data_1_3",
+        word_2: "test-data_2_3",
       },
       {
         rank: 4,
-        word_1: "test-data",
-        word_2: "test-data_2",
+        word_1: "test-data_1_4",
+        word_2: "test-data_2_4",
       },
       {
         rank: 5,
-        word_1: "test-data",
-        word_2: "test-data_2",
+        word_1: "test-data_1_5",
+        word_2: "test-data_2_5",
       },
     ]);
   }, []);
@@ -44,7 +45,7 @@ export default function Dictionarydetail() {
       <Card body className="staticmain-body">
         <h2 className="staticmain-title">통계</h2>
         <hr className="title-body-between" />
-        <div className="staticmain-body">
+        <div className="static-body">
           <table className="static-table">
             <thead>
               <tr>
@@ -57,8 +58,16 @@ export default function Dictionarydetail() {
               {data.map((data, num) => (
                 <tr className="static-row" key={`table_row_${num}`}>
                   <td className="static-rank">{data.rank}</td>
-                  <td className="static-word_1">{data.word_1}</td>
-                  <td className="static-word_2">{data.word_2}</td>
+                  <td className="static-word_1">
+                    <Link to={`/statistic/detail?word=${data.word_1}`}>
+                      <div className="static-word-link">{data.word_1}</div>
+                    </Link>
+                  </td>
+                  <td className="static-word_2">
+                    <Link to={`/statistic/detail?word=${data.word_2}`}>
+                      <div className="static-word-link">{data.word_2}</div>
+                    </Link>
+                  </td>
                 </tr>
               ))}
             </tbody>
