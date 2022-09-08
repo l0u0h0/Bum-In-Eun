1df021ee414c9ac36a6d2234655131edecccf4e9
l0u0h0 <l0u0h0@gmail.com>
2022-09-08 18:21:22 +0900

    CSS_update

--- backend/config/config.js
+++ backend/config/config.js
@@ -4,7 +4,6 @@ dotenv.config();
 const development = {
   dialect: "mssql",
   host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
   username: process.env.USER_NAME,
   password: process.env.USER_PASSWD,
   database: process.env.DB_NAME,
@@ -13,7 +12,6 @@ const development = {
 const production = {
   dialect: "mssql",
   host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
   username: process.env.USER_NAME,
   password: process.env.USER_PASSWD,
   database: process.env.DB_NAME,
@@ -22,7 +20,6 @@ const production = {
 const test = {
   dialect: "mssql",
   host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
   username: process.env.USER_NAME,
   password: process.env.USER_PASSWD,
   database: process.env.DB_NAME,
