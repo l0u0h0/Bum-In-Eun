2067afd12f04d229c8f4fffced3eff1fd7c87e8c
l0u0h0 <l0u0h0@gmail.com>
2022-09-14 21:23:58 +0900

    provider_add

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -11,10 +11,9 @@ import MainComponent from "./components/Maincomponent";
 import SearchComponent from "./components/SearchComponent";
 import Crimemain from "./components/crime/CrimemainComponent";
 import Crimeresult from "./components/crime/CrimeresultComponent";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import { Provider } from "react-redux";
+import { Route, Routes } from "react-router-dom";
 import { HistoryRouter as Router } from "redux-first-history/rr6";
-import { store, history } from "./redux/store";
+import { history } from "./redux/store";
 import Crimedetail from "./components/crime/CrimedetailComponent";
 import DictionaryMain from "./components/dictionary/DicionarymainComponent";
 import Dictionarydetail from "./components/dictionary/DictionarydetailComponent";
@@ -24,26 +23,24 @@ import Statisticdetail from "./components/statistic/StatisticdetailComponent";
 function App() {
   return (
     <div className="App">
-      <Provider store={store}>
-        <Router history={history}>
-          <Routes>
-            {/* MAIN */}
-            <Route path="/" element={<MainComponent />} />
-            {/* SEARCH */}
-            <Route path="/search" element={<SearchComponent />} />
-            {/* CRIME */}
-            <Route path="/crime" element={<Crimemain />} />
-            <Route path="/crime/result" element={<Crimeresult />} />
-            <Route path="/crime/detail" element={<Crimedetail />} />
-            {/* DICTIONARY */}
-            <Route path="/dictionary" element={<DictionaryMain />} />
-            <Route path="/dictionary/detail" element={<Dictionarydetail />} />
-            {/* STATIC */}
-            <Route path="/statistic" element={<Statisticmain />} />
-            <Route path="/statistic/detail" element={<Statisticdetail />} />
-          </Routes>
-        </Router>
-      </Provider>
+      <Router history={history}>
+        <Routes>
+          {/* MAIN */}
+          <Route path="/" element={<MainComponent />} />
+          {/* SEARCH */}
+          <Route path="/search" element={<SearchComponent />} />
+          {/* CRIME */}
+          <Route path="/crime" element={<Crimemain />} />
+          <Route path="/crime/result" element={<Crimeresult />} />
+          <Route path="/crime/detail" element={<Crimedetail />} />
+          {/* DICTIONARY */}
+          <Route path="/dictionary" element={<DictionaryMain />} />
+          <Route path="/dictionary/detail" element={<Dictionarydetail />} />
+          {/* STATIC */}
+          <Route path="/statistic" element={<Statisticmain />} />
+          <Route path="/statistic/detail" element={<Statisticdetail />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
