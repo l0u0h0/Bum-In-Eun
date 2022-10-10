a3a6982f8b6312896476bfff721e80be3b6f7302
l0u0h0 <l0u0h0@gmail.com>
2022-10-10 16:10:11 +0900

    API_key_update

--- frontend/src/components/Maincomponent.tsx
+++ frontend/src/components/Maincomponent.tsx
@@ -248,7 +248,8 @@ function Body2() {
         page_size: "4",
       },
       headers: {
-        "x-api-key": "IgiTs2CNifPgwa1HPCiAakDtu_0yCO__Zb0BJfPMOFA",
+        // "x-api-key": "IgiTs2CNifPgwa1HPCiAakDtu_0yCO__Zb0BJfPMOFA",
+        "x-api-key": "AYl1j-zT4UEs5yA1JjPnj2JqB_H31uZM5IyRMWS_LvY",
       },
     };
     const newsApi = async () => {
