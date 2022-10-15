60afb34f57a5e6d9801327387cdf34aefbe36524
l0u0h0 <l0u0h0@gmail.com>
2022-10-15 19:27:36 +0900

    testing_env

--- frontend/src/service/DataService.ts
+++ frontend/src/service/DataService.ts
@@ -9,6 +9,8 @@ const API_URL =
     ? process.env.REACT_APP_API_URL
     : "http://localhost:3306";
 
+console.log(API_URL);
+console.log(process.env.REACT_APP_NAV_develop);
 // create DataService class
 export default class DataService {
   public static async getDatas(): Promise<Datatype[]> {
